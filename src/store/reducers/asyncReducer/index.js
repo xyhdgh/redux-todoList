9083d957fe2d5444bc6ea09f277b97fb71ad3008
yinghao.xiao <yinghao.xiao@shopee.com>
2020-08-17 10:24:39 +0800

    test first

---
+++ src/store/reducers/asyncReducer/index.js
@@ -0,0 +1,24 @@
+import { ADD_TODO_REQUEST, ADD_TODO_SUCCESS, ADD_TODO_FAILURE } from '../../constants'
+let initailState = {}
+
+const asyncReducer = (state = initailState, action) => {
+  console.log('action::', action)
+  switch (action.type) {
+    case ADD_TODO_REQUEST:
+      return {
+        ...state
+      }
+    case ADD_TODO_SUCCESS:
+      return {
+        ...state
+      }
+    case ADD_TODO_FAILURE:
+      return {
+        ...state
+      }
+    default:
+      return state
+  }
+}
+
+export default asyncReducer
